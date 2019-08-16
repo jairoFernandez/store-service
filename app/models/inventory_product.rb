87eec87d77faf088b8a6f1186ee64b7c2e932164
Jairo Alonso Fernandez Vega <jairo.fernandez@youse.com.co>
2019-08-16 08:48:55 -0500

    fix property quantity

--- app/models/inventory_product.rb
+++ app/models/inventory_product.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# This model is for relation the entities Product, Inventary and keep the quantity
 class InventoryProduct < ApplicationRecord
-    belongs_to :product
-    belongs_to :inventary
-    validates :quantity
+  belongs_to :product
+  belongs_to :inventary
+  validate :quantity
 end
