0c6461c08046d643aa0ae5aa49ff13d77f24f32e
Andres Fernandez <lafvg12@hotmail.com>
2019-08-21 11:51:20 -0500

    se agrego la propiedad de imagen

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :store
   validates :name, :description, presence: true
+  has_one_attached :image
+
+  def image_filename
+    self.image.filename.to_s if self.image.attached?
+  end
+
+  def image_attached?
+    self.image.attached?
+  end
 end
